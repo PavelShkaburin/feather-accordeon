0f63c5167c00a9c820bbb4cd2141a8a4d70ca388
Pavel Shkaburin <smilentkill1945@gmail.com>
2023-06-18 14:29:33 +0300

    publishing

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
-Welcome to the most lightweight accordeon!
+# Welcome to the most lightweight accordeon - feather-accordeon!
+
+> Accordeon! I'm your feather!
+
+## The goals of this script
+
+It's made for common case of using accordeon, when you don't need any options, when you just need an accordeon to do it's main function - to open and close panels by clicking on their control. 
+Also it don't need you to know javaScript for using it. Just include it into your project - and it will work!
+
+## Why it's brilliant? 
+- no parameters
+- no dependences
+- no initialization needed
+- no  unnecessary styles
+- extremely light
+- keyboard navigation support
+- smooth panel opening and closing
+- opened panel don't breaks on window resize
+
+## It won't fit if:
+- you want to customize its behavior with parameters
+- structure of needed accordion is different from  "control is on top, content is under it"
+
+## Getting started
+
+Just download [feather-accordeon.js](https://github.com/PavelShkaburin/feather-accordeon/feather-accordeon.js) & [feather-accordeon.css](https://github.com/PavelShkaburin/feather-accordeon/feather-accordeon.css) or use direct links and include it into your project.
+
+``` html
+  <link href="https://github.com/PavelShkaburin/feather-accordeon/feather-accordeon.css" rel="stylesheet" type="text/css">
+  <script src="https://github.com/PavelShkaburin/feather-accordeon/feather-accordeon.js"> </script>
+```
+
+## Usage
+
+Feather accordeon requires one condition: the html-structure should be like list of items with only two elements inside: control element and content element. Both elements may contain any content. 
+#example:
+```html
+  <ul class="accordeon">
+    <li>
+      <button>Click here to learn more 1</button>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos dolorem libero esse quas aspernatur officia unde laudantium vitae, facere a?
+      </p>
+    </li>
+    <li>
+      <button>Click here to learn more 2</button>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis sint, sunt magni impedit enim ad tempora optio, illum aliquid, vitae nulla repudiandae! Quam consequuntur laboriosam architecto sint repellat officiis dolor!
+      </p>
+    </li>
+    <li>
+      <button>Click here to learn more 3</button>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </p>
+    </li>
+  </ul>
+``` 
+Now  just add class "accordeon" to items wrap element. That's all!
+
+
+
+
+
+
+
---
+++ feather-accordeon.js
@@ -0,0 +1,111 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const accordeons = document.querySelectorAll(".accordeon");
+
+  accordeons.forEach(accordeon => {
+    myAccordeon(accordeon);
+  });
+
+  function myAccordeon(accordeon) {
+    const accordeonItems = accordeon.children;
+    for(item of accordeonItems) {
+      item.classList.add("accordeon__item");
+      item.setAttribute('aria-expanded', false);
+      item.setAttribute('tabindex', 0);
+
+      const itemControl = item.firstElementChild;
+      itemControl.classList.add("accordeon__control");
+
+      const itemPanel = itemControl.nextElementSibling;
+      itemPanel.classList.add("accordeon__panel", "panel-closed");
+      itemPanel.setAttribute('aria-hidden', true);
+    }
+
+    accordeon.querySelector('.accordeon__item').classList.add("accordeon-active");
+    accordeon.setAttribute('aria-expanded', true);
+
+    const firstPanel = accordeon.querySelector('.accordeon-active .accordeon__panel');
+    firstPanel.classList.remove('panel-closed');
+    firstPanel.setAttribute('aria-hidden', false);
+    firstPanel.style.maxHeight = firstPanel.scrollHeight + "px";
+    firstPanel.classList.add('panel-opened');
+
+    function togglePanels(eventTarget){
+      const activeAccordeonItem = accordeon.querySelector('.accordeon-active');
+      if (activeAccordeonItem) {
+        if (activeAccordeonItem === eventTarget.parentNode) {
+          hideAccordeonItem(accordeon)
+        } else {
+          hideAccordeonItem(accordeon);
+          showAccordeonItem(eventTarget)
+        }
+      } else {
+        showAccordeonItem(eventTarget)
+      }
+    }
+
+    accordeon.addEventListener('keydown', function(event) {
+      const eventTab = event.target.parentNode;
+      if (event.target.classList.contains('accordeon__control')) {
+        if ((event.keyCode === 13) || (event.keyCode === 32)) {
+          event.preventDefault();
+          togglePanels(event.target)
+        }
+        if ((event.keyCode === 38) && (eventTab.previousElementSibling !== null)) {
+          event.preventDefault();
+          eventTab.previousElementSibling.firstElementChild.focus()
+        }
+        if ((event.keyCode === 40) && (eventTab.nextElementSibling !== null)) {
+          event.preventDefault();
+          eventTab.nextElementSibling.firstElementChild.focus()
+        }
+      }
+    });
+
+    accordeon.addEventListener('click', (event) => {
+      if (event.target.classList.contains('accordeon__control')) {
+        togglePanels(event.target)
+      }
+    });
+  }
+
+  function showAccordeonItem(accordeonControl) {
+    accordeonControl.parentNode.classList.add('accordeon-active');
+    accordeonControl.parentNode.setAttribute('aria-expanded', true);
+    const panel = accordeonControl.nextElementSibling;
+    panel.classList.remove('panel-closed');
+    panel.setAttribute('aria-hidden', false);
+    panel.classList.add('panel-opened');
+    const scrollHeight = panel.scrollHeight;
+
+    const heightFraction =  scrollHeight / 30;
+
+    let currentHight = 0;
+    let timer = setInterval(() => {
+      currentHight = currentHight + heightFraction;
+      panel.style.maxHeight = currentHight + "px";
+        if (currentHight > scrollHeight) {
+          clearInterval(timer);
+        };
+    }, 1);
+  }
+
+  function hideAccordeonItem(accordeon) {
+    const activeHeader = accordeon.querySelector('.accordeon-active');
+    if (activeHeader) {
+      const activePanel = activeHeader.lastElementChild;
+      activePanel.classList.remove('panel-opened');
+      activePanel.classList.add('panel-closed');
+      activeHeader.setAttribute('aria-expanded', false);
+      activePanel.setAttribute('aria-hidden', true);
+      activePanel.style.maxHeight = null;
+      activeHeader.classList.remove('accordeon-active');
+    }
+  }
+
+  window.addEventListener('resize', () => {
+    const openedPanels = document.querySelectorAll('.accordeon__panel.panel-opened');
+    openedPanels.forEach(panel => {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    });
+  });
+})
